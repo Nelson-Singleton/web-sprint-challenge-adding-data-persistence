09455b5777ac4fbdf2ea40101eb07658eb311380
nelson-singleton <nsingleton@ymail.com>
2020-10-09 10:14:50 -0500

    Added dependencies, adjusted packet.json, create index and server.js, created knexfile template.

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const SchemeRouter = require('./schemes/scheme-router.js');
+
+
+const server = express();
+
+
+server.use(express.json());
+server.use('/api/schemes', SchemeRouter);
+
+
+
+
+module.exports = server;
